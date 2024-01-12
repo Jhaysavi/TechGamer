83219ef0a68bbc0785829a085cefc144cb6d5531
Jhay Savi <jansavinon@outlook.com>
2024-01-11 21:20:28 -0300

    validação formulári

--- src/Paginas/Cadastro/index.jsx
+++ src/Paginas/Cadastro/index.jsx
@@ -2,18 +2,21 @@ import { useEffect, useState } from "react";
 import styles from './Cadastro.module.css';
 import CardLista from "../../Componentes/CardLista";
 import Footer from "../../Componentes/Footer";
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from "yup";
 
 function Cadastro() {
     const [produtos, setProdutos] = useState([]);
 
-  useEffect(() => {
-    fetch(`https://my-json-server.typicode.com/Jhaysavi/techGamer-api/db`)
-      .then(resposta => resposta.json())
-      .then(dados => {
-        setProdutos(dados?.produtos || [])
-      })
-  }, [])
-  
+    useEffect(() => {
+        fetch(`https://my-json-server.typicode.com/Jhaysavi/techGamer-api/db`)
+            .then(resposta => resposta.json())
+            .then(dados => {
+                setProdutos(dados?.produtos || [])
+            })
+    }, [])
+
     const [produto, setProduto] = useState({
         id: '',
         nome: '',
@@ -49,21 +52,45 @@ function Cadastro() {
         });
     };
 
+    const validationSchema = yup.object({
+        nome: yup
+            .string()
+            .required("Nome is required"),
+        preco: yup
+            .string()
+            .required("Preço is required"),
+        descricao: yup
+            .string()
+            .required("Descrição is required"),
+        estoque: yup
+            .string()
+            .required("Estoque is required"),
+        imagem: yup
+            .string()
+            .required("Imagem is required"),
+        cor: yup
+            .string()
+            .required("Cores is required"),
+    });
+
     return (
         <>
 
-        <header className={styles.containerCabecalho}>
-            <img src="/logo.png" alt="Logo" className={styles.logo}/>
-            <h1 className={styles.cadastro}>Sistema de Cadastro</h1>
-            
+            <header className={styles.containerCabecalho}>
+                <img src="/logo.png" alt="Logo" className={styles.logo} />
+                <h1 className={styles.cadastro}>Sistema de Cadastro</h1>
+            </header>
 
-        </header>
+            <Formik 
+                initialValues={{ nome: "", preco: "", descricao: "", estoque: "", imagem:"" , cor:""}}
+                validationSchema={validationSchema}
+            >
             <section className={styles.container}>
                 <h1 className={styles.titulo}>Cadastro de produtos</h1>
                 <form action="/" method='POST' onSubmit={lidarSubmit}>
                     <fieldset className={styles.formGroup}>
                         <label className={styles.label} htmlFor="nome">Nome:</label>
-                        <input   
+                        <Field
                             className={styles.input}
                             type="text"
                             name="nome"
@@ -71,11 +98,12 @@ function Cadastro() {
                             value={produto.nome}
                             onChange={alteracao}
                         />
+                        <ErrorMessage component="p" name="nome" />
                     </fieldset>
 
                     <fieldset className={styles.formGroup}>
                         <label className={styles.label} htmlFor="preco">Preço:</label>
-                        <input
+                        <Field
                             className={styles.input}
                             type="number"
                             name="preco"
@@ -84,11 +112,13 @@ function Cadastro() {
                             onChange={alteracao}
                             required
                         />
+                        <ErrorMessage component="p" name="preco" />
                     </fieldset>
 
                     <fieldset className={styles.formGroup}>
                         <label className={styles.label} htmlFor="descricao">Descrição:</label>
-                        <textarea  
+                        <Field
+                            as="textarea"
                             className={styles.descricao}
                             name="descricao"
                             id="descricao"
@@ -96,14 +126,14 @@ function Cadastro() {
                             onChange={alteracao}
                             cols="30"
                             rows="10"
-                            required>
-                           
-                        </textarea> 
+                            required
+                        />
+                        <ErrorMessage component="p" name="descricao" />
                     </fieldset>
 
                     <fieldset className={styles.formGroup}>
                         <label className={styles.label} htmlFor="estoque">Estoque:</label>
-                        <input
+                        <Field
                             className={styles.input}
                             type="number"
                             name="estoque"
@@ -112,11 +142,12 @@ function Cadastro() {
                             onChange={alteracao}
                             required
                         />
+                        <ErrorMessage component="p" name="estoque" />
                     </fieldset>
 
                     <fieldset className={styles.formGroup}>
                         <label className={styles.label} htmlFor="imagem">Imagem</label>
-                        <input
+                        <Field
                             className={styles.input}
                             type="file"
                             name="imagem"
@@ -129,7 +160,7 @@ function Cadastro() {
 
                     <fieldset className={styles.formGroup}>
                         <label className={styles.label} htmlFor="cor">Cores</label>
-                        <input
+                        <Field
                             className={styles.input}
                             type="text"
                             name="cor"
@@ -137,12 +168,13 @@ function Cadastro() {
                             value={produto.cor}
                             onChange={alteracao}
                         />
+                        <ErrorMessage component="p" name="cor" />
                     </fieldset>
 
                     <button type="submit" className={styles.botao}>Cadastrar produtos</button>
                 </form>
             </section>
-
+            </Formik>
             <section>
                 <h2 className={styles.titulo}>Produtos cadastrados</h2>
 
